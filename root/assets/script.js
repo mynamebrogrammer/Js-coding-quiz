928ba41e079e41dc8dfd556dccab453d6c0f9e74
Habib Maksoud <habibmaksoud@gmail.com>
2023-01-09 19:40:20 -0800

    changed event listener to function on start click

--- root/assets/script.js
+++ root/assets/script.js
@@ -1,72 +1,93 @@
-
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
     var time = 60;
     var timer;
     var quiz = [
-        {question: "What is the purpose of the `var` keyword in JavaScript?",
-        choices: [
-            "To declare a variable",
-            "To assign a value to a variable",
-            "To create a function",
-            "To end a block of code"
-        ],
-        answer: "To declare a variable"
+        {
+            question:
+                "What is the purpose of the `var` keyword in JavaScript?",
+            choices: [
+                "To declare a variable",
+                "To assign a value to a variable",
+                "To create a function",
+                "To end a block of code"
+            ],
+            answer: "To declare a variable"
         },
         {
-        question: "What is the difference between a `for` loop and a `while` loop in JavaScript?",
-        choices: [
-            "A `for` loop is used to execute a block of code repeatedly, while a `while` loop is used to execute a block of code a specified number of times",
-            "A `for` loop is used to execute a block of code a specified number of times, while a `while` loop is used to execute a block of code repeatedly",
-            "There is no difference between `for` and `while` loops in JavaScript",
-            "`for` loops and `while` loops cannot be used in JavaScript"
-        ],
-        answer: "A `for` loop is used to execute a block of code a specified number of times, while a `while` loop is used to execute a block of code repeatedly"
+            question:
+                "What is the difference between a `for` loop and a `while` loop in JavaScript?",
+            choices: [
+                "A `for` loop is used to execute a block of code repeatedly, while a `while` loop is used to execute a block of code a specified number of times",
+                "A `for` loop is used to execute a block of code a specified number of times, while a `while` loop is used to execute a block of code repeatedly",
+                "There is no difference between `for` and `while` loops in JavaScript",
+                "`for` loops and `while` loops cannot be used in JavaScript"
+            ],
+            answer:
+                "A `for` loop is used to execute a block of code a specified number of times, while a `while` loop is used to execute a block of code repeatedly"
         }
     ];
     function startTimer() {
         var score = 0;
         var currQuestion = 0;
-
+    
         clearInterval(timer);
         timer = setInterval(function () {
-            if (currQuestion >= quiz.length) {
-                clearInterval(timer);
-                $(".timer").html(`You got ${score} out of ${quiz.length} questions correct!`);
-                return;
+          if (currQuestion >= quiz.length) {
+            clearInterval(timer);
+            document.querySelector(".timer").innerHTML =
+              "You got " +
+              score +
+              " out of " +
+              quiz.length +
+              " questions correct!";
+            return;
+          }
+          time--;
+          console.log("Timer updated: " + time);
+    
+          document.querySelector(".timer").innerHTML = time + "s ";
+    
+          if (time <= 0) {
+            clearInterval(timer);
+            document.querySelector(".timer").innerHTML = "Time is up!";
+            return;
+          }
+    
+          var question = quiz[currQuestion].question;
+          var choices = quiz[currQuestion].choices;
+          var answer = quiz[currQuestion].answer;
+    
+          document.querySelector(".question").innerHTML = question;
+          document.querySelector(".choices").innerHTML = "";
+          for (let i = 0; i < choices.length; i++) {
+            document.querySelector(
+              ".choices"
+            ).innerHTML +=
+              '<input type="radio" name="choice" value="' +
+              choices[i] +
+              '"> ' +
+              choices[i] +
+              "<br>";
+          }
+    
+          document.querySelector(".submit-button").addEventListener("click", function () {
+            let userAnswer = document.querySelector(
+              "input[name='choice']:checked"
+            ).value;
+    
+            if (userAnswer === answer) {
+              score++;
             }
-            time--;
-            console.log(`Timer updated: ${time}`);
-
-            $(".timer").html(time + "s ");
-
-            if (time <= 0) {
-                clearInterval(timer);
-                $(".timer").html("Time is up!");
-                return;
-            }
-            
-
-            $(".question").html(question);
-            $(".choices").html("");
-            for (let i = 0; i < choices.length; i++) {
-                $(".choices").append(`<input type="radio" name="choice" value="${choices[i]}"> ${choices[i]}<br>`);
-            }
-
-            $(".submit-button").click(function() {
-                let userAnswer = $("input[name='choice']:checked").val();
-
-                if (userAnswer === answer) {
-                    score++;
-                  }
-                  currQuestion++;
-            });
-
+            currQuestion++;
+          });
         }, 1000);
-    }
-
+      }
+      document.querySelector(".start-button").addEventListener("click", startTimer)
+      function init() {
+        
+      }
+      document.addEventListener("DOMContentLoaded", init);
+    });
     function init() {
-        $(".start-button").click(startTimer);
-    }
-    $(document).ready(init);
-});
-
+        document.querySelector(".start-button").addEventListener("click", startTimer);
+      }
